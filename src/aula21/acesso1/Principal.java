10d5c3cc9f9e24578303cbb80f6cd738544fcb22
Ryzen720 <guilhermedsmarta@gmail.com>
2024-08-28 08:11:31 -0300

    ajuste no 20 e 21

--- src/aula21/acesso1/Principal.java
+++ src/aula21/acesso1/Principal.java
@@ -1,9 +1,8 @@
 package aula21.acesso1;
-
 public class Principal {
     public static void main(String[] args) {
         Pessoa p1 = new Pessoa();
-        p1.nome = "Jonas";
+        p1.nome = "Marta";
         p1.sobrenome = "Silva";
     }
 }
